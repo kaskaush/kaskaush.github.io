8283104328f475baaa56cde8e26fbac4ccc21908
kaushik kasibhatla <kasibhatla.kaushik@sapient.com>
2020-07-15 19:57:45 +0530

    Updates

---
+++ precache-manifest.5f8860c097393a920119bbfa7940eda4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafaf9e927b0505c2f2a3a8bcbbd9065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38dd0f91e04a8dff6937",
+    "url": "/static/css/main.d4863dc8.chunk.css"
+  },
+  {
+    "revision": "ea1f7338b6359bf8b970",
+    "url": "/static/js/2.4d04cd8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d04cd8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38dd0f91e04a8dff6937",
+    "url": "/static/js/main.56a0131d.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  }
+]);
