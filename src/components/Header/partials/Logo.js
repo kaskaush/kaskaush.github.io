5ae8b6a029d47e1bbc40894b789734fe94f81849
Kaushik Kasibhatla <kaushik.kasibhatala@oracle.com>
2021-10-03 19:54:33 +0530

    updated images

--- src/components/Header/partials/Logo.js
+++ src/components/Header/partials/Logo.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Profile from '../../../assets/profile-pic.png';
 
 const Logo = ({ titleLabel, subLabel, ariaLabel }) => {
     return (
         <div className="header-work__logo-wrapper">
+            <div className="header__profile-wrapper">
+                <img src={Profile} alt="profile" className="header__profile-image" />
+            </div>
             <a href="/home" className="header__logo" aria-label={ariaLabel}>
                 <span>{titleLabel}</span>
                 <span>{subLabel}</span>
